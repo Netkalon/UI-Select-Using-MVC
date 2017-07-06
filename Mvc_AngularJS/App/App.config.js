0ecd03a68f6d6ad20d86c1bd1a9fb6571e2ada6f
Myn <Myn@MYLTOP>
2017-07-06 19:35:21 +0530

    Ui select

---
+++ Mvc_AngularJS/App/App.config.js
@@ -0,0 +1,35 @@
+﻿
+uiroute.config(function ($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/list');
+    $stateProvider
+        // State managing  
+         .state('login', {
+             url: '/login',
+             templateUrl: '/App/Test/login.html',
+             controller: 'LoginController'
+         })
+        //Manager Role
+        .state('manager', {
+            url: '/manager',
+            templateUrl: '/App/Manager/home.html'
+        })
+         .state('list', {
+             url: '/list',
+             templateUrl: '/App/Test/dataList.html'
+         })
+         //Manager Role
+        .state('employee', {
+            url: '/employee',
+            templateUrl: '/App/Employee/home.html'
+        })
+        .state('employee.list', {
+            url: '/list',
+            templateUrl: '/App/Test/homelist.html',
+            controller: function ($scope) {
+                $scope.Language = ['C#', 'VB', 'JavaScript', 'PHP'];
+            }
+        });
+        
+       
+});
+
